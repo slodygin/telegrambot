5938557cacc4d4905bd2a839dd9f364287654f9b
Sergey Lodygin <slodygin@gmail.com>
2023-08-02 19:29:23 +0400

    coffeon,coffeein,coffeestatus,dr команды добавлены

--- bot/handlers/admin/main.py
+++ bot/handlers/admin/main.py
@@ -1,6 +1,8 @@
-from aiogram import Dispatcher
+from aiogram import Dispatcher, types
 
 
 def register_admin_handlers(dp: Dispatcher):
     # todo: register all admin handlers
-    pass
+    @dp.message_handler(commands=['dr'])
+    async def send_welcome(message: types.Message):
+      await message.reply("Hi!\nThis command will send congratulations messages to people!")
--- bot/main.py
+++ bot/main.py
@@ -6,12 +6,13 @@ from bot.filters import register_all_filters
 from bot.misc import TgKeys
 from bot.handlers import register_all_handlers
 from bot.database.models import register_models
-
+from bot.database import get_connection
 
 async def __on_start_up(dp: Dispatcher) -> None:
     register_all_filters(dp)
     register_all_handlers(dp)
     register_models()
+    get_connection()
 
 
 def start_bot():
