f41acb738f68dedea415df41b14003be6fbb78c8
osipovpa <p82o@yandex.ru>
2023-08-05 20:24:23 +0300

    refactor admin commands

--- app/handlers/admin.py
+++ app/handlers/admin.py
@@ -1,24 +1,30 @@
+import logging
+
+from aiogram.dispatcher.filters.builtin import Command
 from aiogram.types import Message
 
 from app.controls import dispatcher as dp
 from app.misc import ai
 
-
-@dp.message_handler(commands=["drtext"])
-async def drtext_command(message: Message):
-    arguments = message.get_args()
-    await message.reply("Hi!\nThis command will send congratulations messages to people!")
-    await message.answer(arguments)
-    ret = ai.generate_text(arguments)
-    await message.answer(ret)
+logger = logging.getLogger(__name__)
 
 
-@dp.message_handler(commands=["drimage"])
-async def drimage_command(message: Message):
-    #bot = get_bot()
-    arguments = message.get_args()
-    await message.reply("Hi!\nThis command will send congratulations messages to people!")
-    await message.answer(arguments)
-    url1 = ai.generate_image(arguments)
-    await message.answer(url1)
-    await message.answer_photo(photo=url1)
+@dp.message_handler(Command("drtext"))
+async def drtext_command(message: Message, command: Command.CommandObj):
+    logger.debug(command.args)
+    ret = ai.generate_text(command.args)
+    if ret is not None:
+        await message.answer(ret)
+    else:
+        await message.answer("Возникли какие-то проблемы. Но мы скоро всё починим.")
+
+
+@dp.message_handler(Command("drimage"))
+async def drimage_command(message: Message, command: Command.CommandObj):
+    logger.debug(command.args)
+    url1 = ai.generate_image(command.args)
+    if url1 is not None:
+        await message.answer(url1)
+        await message.answer_photo(photo=url1)
+    else:
+        await message.answer("Возникли какие-то проблемы. Но мы скоро всё починим.")
--- app/misc/ai.py
+++ app/misc/ai.py
@@ -1,13 +1,17 @@
+import logging
+
 import openai
 
 from app.misc.env import AI_TOKEN
+
+logger = logging.getLogger(__name__)
 
 openai.api_key = AI_TOKEN
 messages = [{"role": "system", "content": "You are a intelligent assistant."}]
 
 
-def generate_text(message):
-    if message:
+def generate_text(message: str | None):
+    if message and openai.api_key is not None:
         messages.append(
             {"role": "user", "content": message},
         )
@@ -15,17 +19,18 @@ def generate_text(message):
             model="gpt-3.5-turbo", messages=messages
         )
         reply = chat.choices[0].message.content
-        #print(f"ChatGPT: {reply}")
+        logger.debug(f"ChatGPT: {reply}")
         messages.append({"role": "assistant", "content": reply})
         return reply
 
 
-def generate_image(text):
-    res = openai.Image.create(
-        prompt=text,
-        n=1,
-        size="512x512",
-    )
-    # returning the URL of one image as
-    # we are generating only one image
-    return res["data"][0]["url"]
+def generate_image(text: str | None):
+    if text and openai.api_key is not None:
+        res = openai.Image.create(
+            prompt=text,
+            n=1,
+            size="512x512",
+        )
+        # returning the URL of one image as
+        # we are generating only one image
+        return res["data"][0]["url"]
