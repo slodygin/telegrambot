4ba80cb56738320d4781edcb419d304da39f6008
osipovpa <p82o@yandex.ru>
2023-08-05 20:08:10 +0300

    refactoring

--- app/handlers/user.py
+++ app/handlers/user.py
@@ -1,5 +1,6 @@
 import logging
 
+from aiogram.dispatcher.filters.builtin import Command
 from aiogram.types import Message
 
 from app.controls import dispatcher as dp
@@ -8,20 +9,18 @@ from app.database import coffee
 logger = logging.getLogger(__name__)
 
 
-# coffeon
-@dp.message_handler(commands=["coffeeon"])
-async def coffeeon_command(message: Message):
-    if coffee.change_status(message.from_user.id, coffee.CoffeeStatus.ON):
-        await message.answer("Успешно установлен статус on.")
-    else:
-        await message.answer("Возникли какие-то проблемы. Но мы скоро всё починим.")
+command_target_status = {
+    "coffeeon": coffee.CoffeeStatus.ON,
+    "coffeeoff": coffee.CoffeeStatus.OFF
+}
 
 
-# coffeeoff
-@dp.message_handler(commands=["coffeeoff"])
-async def coffeeoff_command(message: Message):
-    if coffee.change_status(message.from_user.id, coffee.CoffeeStatus.OFF):
-        await message.answer("Успешно установлен статус off.")
+# coffeon / coffeeoff
+@dp.message_handler(Command(commands=("coffeeon", "coffeeoff")))
+async def coffeeon_command(message: Message, command: Command.CommandObj):
+    target_status = command_target_status[command.command]
+    if coffee.change_status(message.from_user.id, target_status):
+        await message.answer(f"Успешно установлен статус {target_status.name}.")
     else:
         await message.answer("Возникли какие-то проблемы. Но мы скоро всё починим.")
 
@@ -29,7 +28,5 @@ async def coffeeoff_command(message: Message):
 #coffeestatus
 @dp.message_handler(commands=["coffeestatus"])
 async def coffeestatus_command(message: Message):
-    if coffee.get_status(message.from_user.id) == coffee.CoffeeStatus.ON:
-        await message.answer("Текущий статус on")
-    else:
-        await message.answer("Текущий статус off")
+    status = coffee.get_status(message.from_user.id)
+    await message.answer(f"Текущий статус: {status.name}")
